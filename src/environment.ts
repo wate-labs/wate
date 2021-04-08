a427d55aad0d6f72fb2830ce8729da26cfee18a4
Flo Schuessel <mail@florian-schuessel.de>
2021-04-08 12:36:36 +0200

    Add environment loader

---
+++ src/environment.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs'
+
+export default class EnvironmentLoader {
+  public static load(filePath: string): Environment {
+    if (!fs.existsSync(filePath)) {
+      throw new Error(`File ${filePath} not found`)
+    }
+    const content = JSON.parse(fs.readFileSync(filePath).toString())
+    const scheme = content.scheme ?? 'https'
+    const host = content.host
+
+    return {
+      scheme,
+      host,
+    }
+  }
+}
+
+interface Environment {
+  scheme: string;
+  host: string;
+}
