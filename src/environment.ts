bd8891818e26c2faa7efc6c8c50d862b72cbbd59
Flo Schuessel <mail@florian-schuessel.de>
2022-08-23 16:17:34 +0200

    Extract running cases and requests to own methods

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,5 @@
 export default interface Environment {
+  name: string;
   scheme: string;
   host: string;
 }
--- src/environment/loader.ts
+++ src/environment/loader.ts
@@ -35,6 +35,7 @@ export default class EnvironmentLoader {
       }
 
       return {
+        name,
         scheme,
         host,
       }
